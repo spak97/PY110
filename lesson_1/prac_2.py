766cd069fbde4812a9d477a5c961650bb35fd3cf
Sang Pak <spak34@berkeley.edu>
2024-05-21 17:35:15 -0700

    5/21/23

---
+++ lesson_1/prac_2.py
@@ -0,0 +1,18 @@
+# produce = {
+#     'apple': 'Fruit',
+#     'carrot': 'Vegetable',
+#     'pear': 'Fruit',
+#     'broccoli': 'Vegetable',
+# }
+
+# def select_fruit(hash):
+#     res = {}
+#     for k in hash:
+#         if hash[k] == "Fruit":
+#             res[k] = hash[k]
+    
+#     return res
+
+# print(select_fruit(produce))  # { apple: 'Fruit', pear: 'Fruit' }
+
+
